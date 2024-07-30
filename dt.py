59a813348012fae1bb48409cacc4664ef6ae962d
jerhugtech <Jerome.Hughes@gmail.com>
2024-07-30 11:05:37 -0400

    Update dt.py
    change to path

--- dt.py
+++ dt.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 
-with open("../../input.txt", "r") as f:
+with open(".\input.txt", "r") as f:
     content = f.readlines()
     print(content)
 
 file_name = str(content)    
-with open("../../output.txt", "w") as f:
+with open(".\output.txt", "w") as f:
     f.write(file_name + '\n')
     f.close()
