51224735d3b3568bbf70375de80b857c535793c4
jerhugtech <Jerome.Hughes@gmail.com>
2024-07-30 10:34:27 -0400

    Update dt.py

--- dt.py
+++ dt.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 
-with open("C:/Users/Jerome/Desktop/wtpython/input.txt", "r") as f:
+with open("../../input.txt", "r") as f:
     content = f.readlines()
     print(content)
 
 file_name = str(content)    
-with open("C:/Users/Jerome/Desktop/wtpython/output.txt", "w") as f:
+with open("../../output.txt", "w") as f:
     f.write(file_name + '\n')
     f.close()
