93e668f317a80e01bfc4ac7451ed40a3c7b98af4
jerhugtech <Jerome.Hughes@gmail.com>
2024-07-30 13:33:36 -0400

    Update JHDT.py

--- JHDT.py
+++ JHDT.py
@@ -2,7 +2,7 @@ import re
 
 def is_valid_datetime(datetime_str):
 # Regular expression pattern for ISO 8601 date-time format
-  pattern = r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(Z|[+-]\d{2}:\d{2})$&";
+  pattern = r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(Z|[+-]\d{2}:\d{2})$";
   return re.match(pattern, datetime_str) is not None
 
 def read_datetime_values(file_path):
